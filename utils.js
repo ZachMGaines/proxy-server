eb69b96815980009af36d7e42ebc0e2ecfc393e2
Zachary Gaines <zacharygaines28@gmail.com>
2021-05-14 16:52:52 -0700

    weather working

--- utils.js
+++ utils.js
@@ -1,7 +1,16 @@
-export function mungeLocation(booger) {
+export default function mungeLocation(booger) {
   return {
     formatted_query: booger[0].display_name,
     latitude: booger[0].lat,
     longitude: booger[0].lon
   };
+}
+
+export function mungeWeather(weather) {
+  return weather.data.map(item => {
+    return {
+      forecast: item.weather.description,
+      time: item.valid_date
+    };
+  });
 }
